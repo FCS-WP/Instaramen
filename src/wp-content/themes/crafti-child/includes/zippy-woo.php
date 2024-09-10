de6092358207953bd1669cbe1da0b3a8e8019039
DevTran <tan.tran@floatingcube.com>
2024-09-10 08:42:54 +0700

    change title send to different address

--- src/wp-content/themes/crafti-child/includes/zippy-woo.php
+++ src/wp-content/themes/crafti-child/includes/zippy-woo.php
@@ -34,3 +34,49 @@ function sentence_signup_get_free_point() {
   echo '<p class="custom-notification">Sign up now and get FREE 500 points - Click <a href="/my-account">here</a> to register </p>';
 }
 
+add_action('woocommerce_checkout_billing', 'add_residential_detail_checkout_page');
+function add_residential_detail_checkout_page(){
+  echo '<h3>Residential details</h3>';
+  echo '<p class="form-row form-row-wide" id="residential_field" data-priority="30"><label for="residential" class="">Another Address&nbsp;<span class="optional">(optional)</span></label><span class="woocommerce-input-wrapper"><input type="text" class="input-text " name="residential" id="residential" placeholder="" value="" autocomplete="residential"></span></p>';
+}
+
+add_action('woocommerce_checkout_update_order_meta', 'save_residential_field_order_meta');
+function save_residential_field_order_meta($order_id) {
+    if ( ! empty( $_POST['residential'] ) ) {
+        update_post_meta( $order_id, 'residential_detail', sanitize_text_field( $_POST['residential'] ) );
+    }
+}
+
+// Display the custom field data in the admin order details
+add_action('woocommerce_admin_order_data_after_billing_address', 'display_residential_field_in_admin_order', 10, 1);
+function display_residential_field_in_admin_order($order){
+    $residential_detail = get_post_meta($order->get_id(), 'residential_detail', true);
+    if ( ! empty( $residential_detail ) ) {
+        echo '<p><strong>Residential Detail:</strong> ' . esc_html($residential_detail) . '</p>';
+    }
+}
+
+
+add_filter( 'gettext', 'ahirwp_translate_woocommerce_strings', 999, 3 );
+  
+function ahirwp_translate_woocommerce_strings( $translated, $untranslated, $domain ) {
+ 
+   if ( ! is_admin() && 'woocommerce' === $domain ) {
+ 
+      switch ( $translated ) {
+ 
+         case 'Ship to a different address?':
+ 
+            $translated = 'Residential Details';
+            break;
+ 
+        
+         // ETC
+       
+      }
+ 
+   }   
+  
+   return $translated;
+ 
+}
