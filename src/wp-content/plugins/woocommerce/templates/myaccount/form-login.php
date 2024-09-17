5c5b539f553bca92d5c89e2bcd7908625a14d871
DevTran <tan.tran@floatingcube.com>
2024-09-17 15:44:34 +0700

    add recapcha for register page

--- src/wp-content/plugins/woocommerce/templates/myaccount/form-login.php
+++ src/wp-content/plugins/woocommerce/templates/myaccount/form-login.php
@@ -86,20 +86,21 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
 				<label for="reg_email"><?php esc_html_e( 'Email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
 				<input type="email" class="woocommerce-Input woocommerce-Input--text input-text" name="email" id="reg_email" autocomplete="email" value="<?php echo ( ! empty( $_POST['email'] ) ) ? esc_attr( wp_unslash( $_POST['email'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
 			</p>
-
+			<?php echo do_shortcode('[bws_google_captcha]'); ?>
 			<?php if ( 'no' === get_option( 'woocommerce_registration_generate_password' ) ) : ?>
 
 				<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 					<label for="reg_password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
 					<input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password" id="reg_password" autocomplete="new-password" />
 				</p>
-
+			
+			
 			<?php else : ?>
 
 				<p><?php esc_html_e( 'A link to set a new password will be sent to your email address.', 'woocommerce' ); ?></p>
 
 			<?php endif; ?>
-
+			
 			<?php do_action( 'woocommerce_register_form' ); ?>
 
 			<p class="woocommerce-form-row form-row">
